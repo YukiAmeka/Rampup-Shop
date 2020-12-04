e02de6e3e0e427b20da62d8d72045bb6e67aa1f8
YukiAmeka <anyalo307@gmail.com>
2020-12-04 16:05:31 +0200

    Refactored master seeding procedure to dynamically go through proc list & exec each

--- Rampup Shop/DataSeeding/Stored Procedures/STP_PopulateMasterData.sql
+++ Rampup Shop/DataSeeding/Stored Procedures/STP_PopulateMasterData.sql
@@ -15,37 +15,48 @@ BEGIN
 	BEGIN TRY
 		DECLARE @OperationRunId INT,
 			@AffectedRows INT,
-			@TotalAffectedRows INT = 0;
+			@TotalAffectedRows INT = 0,
+			@ProcExecString NVARCHAR(MAX),
+			@ProcName VARCHAR(100),
+			@Counter INT = 1,
+			@NumberOfProcs INT;
 
 		-- Log operation start
 		EXEC [Logs].[STP_StartOperation] @CallingProc = @@PROCID,
 			@Message = 'Data seeding is in progress.', 
-			@OperationRunId = @OperationRunId OUTPUT
+			@OperationRunId = @OperationRunId OUTPUT;
 
-		-- Populate data into [Master].[EmployeePositions]
-		EXEC [DataSeeding].[STP_PopulateEmployeePositions] @OperationRunId = @OperationRunId,
-			@AffectedRows = @AffectedRows OUTPUT;
-		SET @TotalAffectedRows += @AffectedRows;
+		-- Record a temporary list of data seeding procedures in the order of execution
+		DROP TABLE IF EXISTS #SeedingProcedures;
+		CREATE TABLE #SeedingProcedures (
+			SeedingProcedureId INT IDENTITY(1,1) NOT NULL,
+			ProcName VARCHAR(100) NOT NULL
+		);
+		INSERT INTO #SeedingProcedures
+			VALUES ('STP_PopulateEmployeePositions'),
+				('STP_PopulateEmployees'),
+				('STP_PopulateCustomers'),
+				('STP_PopulateAddresses'),
+				('STP_PopulateCustomerAddresses');
+		SET @NumberOfProcs = (SELECT COUNT(ProcName) FROM #SeedingProcedures);
 
-		-- Populate data into [Master].[Employees]
-		EXEC [DataSeeding].[STP_PopulateEmployees] @OperationRunId = @OperationRunId,
-			@AffectedRows = @AffectedRows OUTPUT;
-		SET @TotalAffectedRows += @AffectedRows;
+		-- Run data seeding procedures one by one to populate tables with dummy data
+		WHILE @Counter <= @NumberOfProcs
+		BEGIN
+			SET @ProcName = (SELECT ProcName FROM #SeedingProcedures WHERE SeedingProcedureId = @Counter);
+			SET @ProcExecString = N'EXEC [DataSeeding].' + QUOTENAME(@ProcName) + ' @OperationRunId, @AffectedRows OUTPUT;'
 
-		-- Populate data into [Master].[Customers]
-		EXEC [DataSeeding].[STP_PopulateCustomers] @OperationRunId = @OperationRunId,
-			@AffectedRows = @AffectedRows OUTPUT;
-		SET @TotalAffectedRows += @AffectedRows;
+			EXEC sp_executesql @ProcExecString,
+				N'@OperationRunId INT, @AffectedRows INT OUTPUT',
+				@OperationRunId = @OperationRunId,
+				@AffectedRows = @AffectedRows OUTPUT;
+		
+			SET @TotalAffectedRows += @AffectedRows;
+			SET @Counter += 1;
+		END;
 
-		-- Populate data into [Master].[Addresses]
-		EXEC [DataSeeding].[STP_PopulateAddresses] @OperationRunId = @OperationRunId,
-			@AffectedRows = @AffectedRows OUTPUT;
-		SET @TotalAffectedRows += @AffectedRows;
-
-		-- Populate data into [Master].[CustomerAddresses]
-		EXEC [DataSeeding].[STP_PopulateCustomerAddresses] @OperationRunId = @OperationRunId,
-			@AffectedRows = @AffectedRows OUTPUT;
-		SET @TotalAffectedRows += @AffectedRows;
+		-- Drop the temporary list of data seeding procedures
+		DROP TABLE #SeedingProcedures;
 
 		-- Log successful operation completion
 		EXEC [Logs].[STP_CompleteOperation] @OperationRunId = @OperationRunId,
